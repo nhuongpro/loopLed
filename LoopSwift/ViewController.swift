c393d3e0424729d31ef1c6e102192dec4572ab1e
nguyen van nhuong <nguyenvannhuong@viet.local>
2017-03-27 14:01:03 +0700

    Initial Commit

---
+++ LoopSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoopSwift
+//
+//  Created by nguyen van nhuong on 3/27/17.
+//  Copyright © 2017 nguyen van nhuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
