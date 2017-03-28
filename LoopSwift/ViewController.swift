9cd61cdf7e68dbe3c272469c7e5edebe380b5bbe
nguyen van nhuong <nguyenvannhuong@Viet.local>
2017-03-28 16:38:58 +0700

    loop led

--- LoopSwift/ViewController.swift
+++ LoopSwift/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblNumber: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func btnDraw(_ sender: Any) {
+    
+        for views in self.view.subviews{
+            if views.isKind(of: UIImageView.self){
+                views.removeFromSuperview()
+
+            }
+            
+        }
+        
+        let n:Int = Int(lblNumber.text!)!
+        let distances = Distance(numberBall: n)
+        for x in 0..<n {
+            for y in 0..<n {
+                DrawBall(X: 30 + (distances[0] * CGFloat(x)) , Y: 90 + (distances[1] * CGFloat(y)))
+            }
+        }
+        
+        
+    }
+    
+    func Distance(numberBall : Int)->[CGFloat]  {
+        var distanceX:CGFloat!
+        let distanceY:CGFloat!
+        
+        if numberBall == 1 {
+            distanceX = 30
+            distanceY = 30
+        }else{
+            distanceX = (self.view.bounds.size.width - 60 )/CGFloat(numberBall - 1)
+            distanceY = (self.view.bounds.size.height - 120 )/CGFloat(numberBall - 1)
+        }
+        return [distanceX, distanceY]
+    }
+    
+    
+    func DrawBall(X : CGFloat,Y : CGFloat)  {
+            let image = UIImage(named: "green@2x")
+            let imageview = UIImageView(image: image)
+            imageview.center = CGPoint(x: X, y: Y )
+            view.addSubview(imageview)
+            
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
+
+
+
 
